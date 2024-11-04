3155b6bc13725b5dc8744c22e632fee1322bd386
Arya Gupta <artagupta239gmail.com>
2024-11-05 04:44:01 +0530

    database connectivity established

--- server/db.js
+++ server/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI =  "mongodb://root:<your-password>@127.0.0.1:27017";
+const mongoURI =  "mongodb+srv://aryagupta239:6SLlUAMr6aMxhrf0@cluster0.rkrvk.mongodb.net/";
 
 const connectToMongo = async (retryCount) => {
     const MAX_RETRIES = 3;
