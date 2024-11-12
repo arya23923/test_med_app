7b766ec2adaf22bc78e2c219ced46e878ca6d076
Arya Gupta <artagupta239gmail.com>
2024-11-12 18:51:45 +0530

    testing edit in index.js

--- server/index.js
+++ server/index.js
@@ -24,6 +24,10 @@ app.use('/api/auth', require('./routes/auth'));
 
 app.use(express.static(path.join(__dirname, 'build')));
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
 app.get('/', (req, res) => {
     res.send('hello world!');
     // res.sendFile(path.join(__dirname, 'build', 'index.html'));
