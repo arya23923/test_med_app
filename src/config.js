19e41edc2c6eb11facd62c69d7a8489b0053632a
Arya Gupta <artagupta239gmail.com>
2024-11-12 21:32:32 +0530

    works according to ibm instructions

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 export const API_URL = window.location.hostname === "localhost" ? "http://localhost:8181" : "http://localhost:8181";
+// export const API_URL = <add your server side url></add>
 console.log(
     "API_URL :",
     API_URL
