b4a2b319702dfb594a57eb549fd67d6c55d12a3b
Arya Gupta <artagupta239gmail.com>
2024-11-13 02:27:28 +0530

    backend url added

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,5 +1,5 @@
-export const API_URL = window.location.hostname === "localhost" ? "http://localhost:8181" : "http://localhost:8181";
-// export const API_URL = <add your server side url></add>
+// export const API_URL = window.location.hostname === "localhost" ? "http://localhost:8181" : "http://localhost:8181";
+export const API_URL = "https://test-med-app.onrender.com"
 console.log(
     "API_URL :",
     API_URL
